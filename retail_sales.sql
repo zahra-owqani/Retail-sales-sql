863a93cde7b718e0f2c2f2f0cda3c089bd00c4d1
zahra-owqani <102425456+zahra-owqani@users.noreply.github.com>
2025-02-23 00:47:31 +0330

    Update retail_sales.sql

--- retail_sales.sql
+++ retail_sales.sql
@@ -123,6 +123,5 @@ GROUP BY category
  -Customer Demographics: The dataset includes customers from various age groups, 
   with sales distributed across different categories such as Clothing and Beauty.
  -High-Value Transactions: Several transactions had a total sale amount greater than 1000, indicating premium purchases.
- -Sales Trends: Monthly analysis shows variations in sales, helping identify peak seasons.*/
- 
- -Customer Insights: The analysis identifies the top-spending customers and the most popular product categories.
+ -Sales Trends: Monthly analysis shows variations in sales, helping identify peak seasons.
+ -Customer Insights: The analysis identifies the top-spending customers and the most popular product categories.*/
